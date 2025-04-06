6e71f52924369761661bc6f468d1bf120bd979a8
saeed <saaed.amini@gmail.com>
2025-04-06 12:12:05 +0330

    just effects worked at the descktop situation

--- header-3/javascript.js
+++ header-3/javascript.js
@@ -1,32 +1,35 @@
 // scroll-sticky-header
 window.addEventListener('scroll', function () {
-    let boxMenu = document.querySelector('header');
-    let mainMenu = document.querySelector('.second-menu');
-    let itemsMenu = document.querySelectorAll('.menu-link');
-    let whiteLogo = document.querySelector('.whiteLogo');
-    let blackLogo = document.querySelector('.blackLogo');
-    let SearchIcon = document.querySelector('.fa-search');
-    let offcanvasMenu = document.querySelectorAll('.menu-item');
-    if (window.scrollY > 1) {
-        boxMenu.classList.add('fixed');
-        mainMenu.classList.add('backgroundColorHeader');
-        mainMenu.classList.add('fixed');
-        itemsMenu.forEach(item => item.classList.add('itemColor'));
-        blackLogo.style.display = 'none';
-        whiteLogo.style.display = 'block';
-        SearchIcon.style.color = 'white';
+    if (window.innerWidth > 1024) { // Only apply for desktop version
+        let boxMenu = document.querySelector('header');
+        let mainMenu = document.querySelector('.second-menu');
+        let itemsMenu = document.querySelectorAll('.menu-link');
+        let whiteLogo = document.querySelector('.whiteLogo');
+        let blackLogo = document.querySelector('.blackLogo');
+        let SearchIcon = document.querySelector('.fa-search');
+        let offcanvasMenu = document.querySelectorAll('.menu-item');
+        if (window.scrollY > 1) {
+            boxMenu.classList.add('fixed');
+            mainMenu.classList.add('fixed');
+            mainMenu.classList.add('backgroundColorHeader');
+            mainMenu.classList.add('blackRadiusBackground');
+            itemsMenu.forEach(item => item.classList.add('itemColor'));
+            blackLogo.style.display = 'none';
+            whiteLogo.style.display = 'block';
+            SearchIcon.style.color = 'white';
 
-    } else {
-        boxMenu.classList.remove('fixed');
-        mainMenu.classList.remove('backgroundColorHeader');
-        mainMenu.classList.remove('fixed');
-        itemsMenu.forEach(item => item.classList.remove('itemColor'));
-        whiteLogo.style.display = '';
-        blackLogo.style.display = '';
-        SearchIcon.style.color = '';
+        } else {
+            boxMenu.classList.remove('fixed');
+            mainMenu.classList.remove('backgroundColorHeader');
+            mainMenu.classList.remove('fixed');
+            mainMenu.classList.remove('blackRadiusBackground');
+            itemsMenu.forEach(item => item.classList.remove('itemColor'));
+            whiteLogo.style.display = '';
+            blackLogo.style.display = '';
+            SearchIcon.style.color = '';
+        }
     }
-}
-)
+});
 
 // off canvas button
 document.addEventListener("DOMContentLoaded", () => {
